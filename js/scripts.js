ab1c85988a3aa10750228b4bb22cfb551abc5d52
Anthony Brown <asleboeuf@charter.net>
2017-03-15 10:31:27 -0400

    Full address searching

--- js/scripts.js
+++ js/scripts.js
@@ -33,10 +33,18 @@ function initPlMap() {
         if (valLength > maxCount) {
             $this.val($this.val().substring(0, maxCount));
         }
-        if (jQuery("#pac-input").val().length == 5) {
-            var geocoder = new google.maps.Geocoder();
-            getAddressInfoByZip(jQuery("#pac-input").val());
-        }
+        if (jQuery("#pac-input").val().length == 5 && jQuery.isNumeric( jQuery("#pac-input").val()) ) {
+            console.log('is zipcode');
+		    var geocoder = new google.maps.Geocoder();
+          var displayZip =  getAddressInfoByZip(jQuery("#pac-input").val());
+        }else{
+			console.log('is address');
+			 var geocoder = new google.maps.Geocoder();
+            var displayAddress = getAddressInfoByFormatedAddress(jQuery("#pac-input").val());
+			if(displayAddress == false){
+			alert("Please enter a valid address");	
+			}
+		}
         return false;
     });
     // Try HTML5 geolocation.
@@ -351,6 +359,77 @@ function initPlMap() {
     function response(obj) {
         console.log(obj);
     }
+	
+	
+	
+	
+	    // convert the zip code into coordinates and put all the data in an array
+    function getAddressInfoByFormatedAddress(zip) {
+        if (zip.length > 5 && typeof google != 'undefined') {
+            var addr = {};
+            var geocoder = new google.maps.Geocoder();
+            geocoder.geocode({
+                'address': zip
+            }, function(results, status) {
+                if (status == google.maps.GeocoderStatus.OK) {
+                    FadeOut();
+                    if (results.length >= 1) {
+                        for (var ii = 0; ii < results[0].address_components.length; ii++) {
+                            var street_number = route = street = city = state = zipcode = country =
+                                formatted_address = '';
+                            var types = results[0].address_components[ii].types.join(",");
+                            if (types == "street_number") {
+                                addr.street_number = results[0].address_components[ii].long_name;
+                            }
+                            if (types == "route" || types == "point_of_interest,establishment") {
+                                addr.route = results[0].address_components[ii].long_name;
+                            }
+                            if (types == "sublocality,political" || types == "locality,political" ||
+                                types == "neighborhood,political" || types ==
+                                "administrative_area_level_3,political") {
+                                addr.city = (city == '' || types == "locality,political") ? results[0]
+                                    .address_components[ii].long_name : city;
+                            }
+                            if (types == "administrative_area_level_1,political") {
+                                addr.state = results[0].address_components[ii].short_name;
+                            }
+                            if (types == "postal_code" || types ==
+                                "postal_code_prefix,postal_code") {
+                                addr.zipcode = results[0].address_components[ii].long_name;
+                            }
+                            if (types == "country,political") {
+                                addr.country = results[0].address_components[ii].long_name;
+                            }
+                        }
+                        addr.lat = results[0].geometry.location.lat();
+                        addr.lng = results[0].geometry.location.lng();
+                        addr.place = results[0];
+                        addr.success = true;
+						console.log(addr);
+                        setLocationMarkers(addr);
+                    } else {
+						alert("Please enter a valid address");	
+						
+                        response({
+                            success: false
+                        });
+                    }
+                } else {
+					alert("Please enter a valid address");	
+                    response({
+                        success: false
+                    });
+                }
+            });
+        } else {
+			alert("Please enter a valid address");	
+            response({
+                success: false
+            });
+        }
+    }
+	
+	
     // convert the zip code into coordinates and put all the data in an array
     function getAddressInfoByZip(zip) {
         if (zip.length >= 5 && typeof google != 'undefined') {
